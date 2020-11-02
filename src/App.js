48ed6bbd4dd27470ef952e5ea41d0a6e9965b7a2
Aeron Roemer <aeronroemer@users.noreply.github.com>
2020-11-01 21:44:42 -0700

    useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+
 function App() {
+  const [ score, setScore ] = useState(0);
+  const [ message, setMessage ] = useState('Welcome!');
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome!</h1>
+        <h1>{ score }</h1>
+        <button onClick={() => setScore(prevScore => prevScore + 1)}>
+          Increase score
+        </button>
+        <h2>{ message }</h2>
       </header>
     </div>
   );
